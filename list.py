e26ad62902ae259409c65a55c8302857303a00e4
vaishnavibpatil <116648404+vaishnavibpatil@users.noreply.github.com>
2024-08-07 22:36:49 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,17 @@
+a=[12,33,44,56,7,8,12,1,33]
+s="abc"
+print(a)
+a.append(34)
+print(a)
+print(a.count(12))
+a.extend(s)
+print(a)
+print(a.index(56))
+a.insert(2,100)
+print(a)
+a.pop()
+print(a)
+a=[23,44,32,44,1,22,14,53,5]
+print(a)
+a.sort()
+print(a)
